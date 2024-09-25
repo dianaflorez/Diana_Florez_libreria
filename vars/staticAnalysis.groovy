d77024e8e027e15a3cce204f4e096d0b9dd72bc9
dianaflorez <dianaflorezbravo@gmail.com>
2024-09-25 14:59:15 -0500

    new libreary

---
+++ vars/staticAnalysis.groovy
@@ -0,0 +1,24 @@
+def call(boolean abortOnQualityGate = false, boolean abortPipeline = false) {
+    echo "Ejecución de las pruebas de calidad de código"
+    
+    withSonarQubeEnv('SonarQube') {
+        sh 'echo "Simulando ejecución de análisis estático..."'
+    }
+    
+    timeout(time: 5, unit: 'MINUTES') {
+        echo "Simulación de evaluación del Quality Gate"
+        
+        if (abortOnQualityGate) {
+            echo "Quality Gate ha fallado. Abortando pipeline..."
+            error("Pipeline abortado debido a fallo en Quality Gate.")
+        }
+    }
+    
+    // Si el parámetro `abortPipeline` es true, abortar el pipeline manualmente
+    if (abortPipeline) {
+        echo "Abortando pipeline según la configuración..."
+        error("Pipeline abortado manualmente según el parámetro.")
+    }
+    
+    echo "Análisis de código completado correctamente."
+}
