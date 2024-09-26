caf2b2b6e179d18d34bedff2d9fd91ea0d9c817f
dianaflorez <dianaflorezbravo@gmail.com>
2024-09-25 22:26:19 -0500

    [test]

--- vars/staticAnalysis.groovy
+++ vars/staticAnalysis.groovy
@@ -2,14 +2,14 @@ def call(boolean abortOnQualityGate = false, boolean abortPipeline = false) {
     echo "Ejecución de las pruebas de calidad de código"
    
     
-    // timeout(time: 5, unit: 'MINUTES') {
-    //     echo "Simulación de evaluación del Quality Gate"
+    timeout(time: 5, unit: 'MINUTES') {
+        echo "Simulación de evaluación del Quality Gate"
         
-    //     if (abortOnQualityGate) {
-    //         echo "Quality Gate ha fallado. Abortando pipeline..."
-    //         error("Pipeline abortado debido a fallo en Quality Gate.")
-    //     }
-    // }
+        if (abortOnQualityGate) {
+            echo "Quality Gate ha fallado. Abortando pipeline..."
+            error("Pipeline abortado debido a fallo en Quality Gate.")
+        }
+    }
     
     // // Si el parámetro `abortPipeline` es true, abortar el pipeline manualmente
     // if (abortPipeline) {
